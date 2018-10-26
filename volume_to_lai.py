6be3d8dca641dfc171203fdc10dcd3cc630224b0
Daniel Regenass <danielregenass@Daniels-MacBook-Air.local>
2018-10-26 22:57:54 +0200

    draft preprocessing class

---
+++ volume_to_lai.py
@@ -0,0 +1,26 @@
+#!/bin/python
+class Preprocess(object):
+    """docstring forPreprocess."""
+
+    def __init__(self, arg):
+        pass
+
+
+
+    #convert green volume to LAI
+
+
+    def load_greenvolume(path):
+        pass
+
+
+    def load_landclass(path):
+        pass
+
+
+    def convert(greenvolume):
+        pass
+
+
+    def output(path):
+        pass
